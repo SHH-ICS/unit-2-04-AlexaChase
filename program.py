cae6a3042850ac0806a3f303f010d82cd0cffaea
AlexaChase <124379295+AlexaChase@users.noreply.github.com>
2023-03-09 01:38:07 +0000

    new changes

--- program.py
+++ program.py
@@ -2,15 +2,18 @@
 # Task:  use if statements to output the result of the game fizzbuzz.  
 # Start at 1
 # For multiples of 3, output   Fizz
-myNumber = int(input())
-if myNumber = (%3==0)
-  print('Fizz')
 # For multiples of 5, output   Buzz
 # For multiples of 15, output   FizzBuzz
 # End at 32
 
 result = ""
 for myNumber in range(32):
+  if myNumber% 3==0:
+    print('Fizz')
+  elif myNumber% 5==0:
+    print('Buzz')
+  elif myNumber% 15==0:
+    print('FizzBuzz')
   result = str(result) + str(myNumber) + "\n"
 
 print(result)
